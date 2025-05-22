2b1decabaebf51cda3391a483ded97a6b43fd534
Himanshu <138096465+NiceHIMANSHU@users.noreply.github.com>
2025-05-22 17:55:06 +0530

    Add files via upload

---
+++ My Portfolio/README.md
@@ -0,0 +1,3 @@
+# Codsoft-Portfolio
+
+This is my Portfolio website in which i use Html, Css and Javascript for designing my Portfolio website.
---
+++ My Portfolio/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Python Programmer", "Computer Engineer ","Web Designer" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+function change_text(){
+    document.getElementById("but").innerHTML="Downloaded!";
+}
